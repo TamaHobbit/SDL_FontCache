b38135bd5e1645ef973a885a52473647ddd79c13
Tama <t.mcglinn@gmail.com>
2018-08-10 22:36:36 +0200

    Added Makefiles for .o and for test. fixed #includes for SDL2

---
+++ Makefile
@@ -0,0 +1,2 @@
+SDL_FontCache.o: SDL_FontCache.c
+	clang -c SDL_FontCache.c
--- test/main.c
+++ test/main.c
@@ -1,5 +1,5 @@
-#include "SDL.h"
-#include "SDL_FontCache.h"
+#include <SDL2/SDL.h>
+#include "../SDL_FontCache.h"
 
 #include <math.h>
 #include <string.h>
